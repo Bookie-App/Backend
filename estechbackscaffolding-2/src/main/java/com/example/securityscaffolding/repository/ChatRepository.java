3cadeeddaac6e3ab4264d4668285fd98a2b26ec1
Inigo-Lab <inigo.acosta@escuelaestech.es>
2024-04-13 12:04:08 +0200

    chat y mensaje repository

--- estechbackscaffolding-2/src/main/java/com/example/securityscaffolding/repository/ChatRepository.java
+++ estechbackscaffolding-2/src/main/java/com/example/securityscaffolding/repository/ChatRepository.java
@@ -1,11 +1,14 @@
 package com.example.securityscaffolding.repository;
 
 import com.example.securityscaffolding.model.entity.Chats;
+import com.example.securityscaffolding.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chats, Long> {
 
-
+    List<Chats> findByUsuarioEmisorOrUsuarioReceptor(Usuario usuarioEmisor, Usuario usuarioReceptor);
 }
