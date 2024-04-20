c4f253a0aefa0b49fb83071aa9630762af342092
Inigo-Lab <inigo.acosta@escuelaestech.es>
2024-04-20 18:20:51 +0200

    Libro DTO y Libro converter

---
+++ estechbackscaffolding-2/src/main/java/com/example/securityscaffolding/dto/LibroDTO.java
@@ -0,0 +1,21 @@
+package com.example.securityscaffolding.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibroDTO {
+
+    private String titulo;
+    private String autor;
+    private Integer numeroPaginas;
+    private String genero;
+    private String foto;
+    private String sinopsis;
+    private String editorial;
+}
