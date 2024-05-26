d5ba5f886ac5ed7ef89d55251b60083c579b7f45
Inigo-Lab <inigo.acosta@escuelaestech.es>
2024-05-26 11:59:16 +0200

    mensaje de error en solicitudes 403

--- estechbackscaffolding-2/src/main/java/com/example/securityscaffolding/security/config/SecurityConfig.java
+++ estechbackscaffolding-2/src/main/java/com/example/securityscaffolding/security/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.securityscaffolding.security.config;
 
+import com.example.securityscaffolding.security.CustomAuthenticationEntryPoint;
 import com.example.securityscaffolding.security.filters.JwtAuthenticationFilter;
 import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -25,22 +29,38 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthenticationFilter jwtAuthFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    @Autowired
+    private CustomAuthenticationEntryPoint authenticationEntryPoint;
 
-          http
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests( auth -> {
-                    auth.requestMatchers("/api/auth/**", "/v3/api-docs/**" , "/swagger-ui/**", "/swagger-ui.html").permitAll();
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .sessionManagement( session ->{
+                .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .authenticationProvider(authenticationProvider)
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer
+                        .accessDeniedHandler(accessDeniedHandler())
+                        .authenticationEntryPoint(authenticationEntryPoint))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-             return http.build();
+        return http.build();
 
 
     }
+
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("{ \"error\": \"Forbidden\", \"message\": \"No tienes permiso para acceder a esta página.\" }");
+        };
+    }
+
+
 }
